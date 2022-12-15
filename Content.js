94d7cc3e08543967c22d31a9a47a7026a0822bb1
Allan Mathenge <allanmathenge2@gmail.com>
2022-12-15 10:56:41 +0300

    commiting modified files

--- Content.js
+++ Content.js
@@ -34,26 +34,26 @@ const Content = () => {
   return (
     <main>
       {items.length?(
-        <ul>
-          {items.map((item) => (
-            <li className="item" key ={item.id}>
-              <input
-                 type="checkbox"
-                 onChange={() => handleCheck(item.id)}
-                 checked = {item.checked}
-                />
-                <label
-                  style = {(item.checked) ? {textDecoration: "line-through"}: null}
-                  onDoubleClick={() => handleCheck(item.id)}
-                >{item.item}</label>
-                <FaTrashAlt
-                  onClick = {() => handleDelete(item.id)}
-                  role="button"
-                  tabIndex="0"
-                />
-            </li>
-          ))}
-        </ul>
+          <ul>
+            {items.map((item) => (
+              <li className="item" key ={item.id}>
+                <input
+                  type="checkbox"
+                  onChange={() => handleCheck(item.id)}
+                  checked = {item.checked}
+                  />
+                  <label
+                    style = {(item.checked) ? {textDecoration: "line-through"}: null}
+                    onDoubleClick={() => handleCheck(item.id)}
+                  >{item.item}</label>
+                  <FaTrashAlt
+                    onClick = {() => handleDelete(item.id)}
+                    role="button"
+                    tabIndex="0"
+                  />
+              </li>
+            ))}
+          </ul>
         ) : (<p style ={{marginTop: "2rem"}}>List is Empty!</p>)}
     </main>
   )
