55bbe9056aae061cf288b40dc710ab0e0b0ec698
Allan Mathenge <allanmathenge2@gmail.com>
2022-12-15 14:23:58 +0300

    committing an update files

--- Header.js
+++ Header.js
@@ -1,10 +1,14 @@
-const Header = (props) => {
+const Header = ({title}) => {
 
   return (
     <header>
-        <h1>{props.title}</h1>
+        <h1>{title}</h1>
     </header>
   )
+}
+
+Header.defaultProps = {
+  title: "Default Title"
 }
 
 export default Header;
